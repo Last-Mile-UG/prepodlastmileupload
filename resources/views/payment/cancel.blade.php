05ab519f96a4a11f592c561796238bbd820a368b
Ilya Lezhnev <ilezhnev@axmit.com>
2021-04-22 12:39:54 +0300

    Add pages for success and failed orders

--- resources/views/payment/cancel.blade.php
+++ resources/views/payment/cancel.blade.php
@@ -1,1 +1,27 @@
-я пыталься
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="{{ asset('assets/site/css/main.css') }}">
+  <title>{{__('msg.orderFailed')}}</title>
+</head>
+<body>
+  <div class="order-response__page">
+    <img src="{{asset('assets/site/img/logo.png')}}" alt="" class="img-fluid">
+    <h3>
+      {{__('msg.orderFailed')}}
+    </h3>
+  </div>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script>
+    $(document).ready(() => {
+      setTimeout(() => {
+        login = `{{auth()->check()}}`
+        window.location.replace(login ? '/home' : '/');
+      }, 3000)
+    })
+  </script>
+</body>
+</html>
